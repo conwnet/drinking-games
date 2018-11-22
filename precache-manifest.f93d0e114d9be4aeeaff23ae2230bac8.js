64d3d220ab1a99e001b832df9e87ace8e397b01c
netcon <netcon@live.com>
2018-11-22 23:27:42 +0800

    online demo

---
+++ precache-manifest.f93d0e114d9be4aeeaff23ae2230bac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff790cd4c7ac6c6a6f62",
+    "url": "/drinking-games/static/js/runtime~main.ff790cd4.js"
+  },
+  {
+    "revision": "badda28f35ec985877ff",
+    "url": "/drinking-games/static/js/main.badda28f.chunk.js"
+  },
+  {
+    "revision": "a5686bc51a547225ee7e",
+    "url": "/drinking-games/static/js/1.a5686bc5.chunk.js"
+  },
+  {
+    "revision": "badda28f35ec985877ff",
+    "url": "/drinking-games/static/css/main.5ea1d0dd.chunk.css"
+  },
+  {
+    "revision": "4449b3e288bbfd4a04756aed5b4858dd",
+    "url": "/drinking-games/index.html"
+  }
+];
--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Brag from './Brag';
-
-const App = () => <Brag />
-
-export default App;
